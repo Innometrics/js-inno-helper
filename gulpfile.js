679e3b1a92db7ea074ba0920a10517b9dc70438f
Kuvakin Sergey <kuvakin@gmail.com>
2015-10-15 15:21:24 +0300

    refs #8904 added style and syntax validator

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var chalk = require('chalk');
 var csso = require('gulp-csso');
 var filever = require('gulp-version-filename');
 var gulp = require('gulp');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
@@ -13,10 +15,25 @@ function errorHandler (error) {
     return console.log(chalk.red(error.message));
 }
 
+var paths = {
+    js: ['inno-helper.js'],
+    scss: ['inno-theme.scss']
+};
+
 function js () {
-    return gulp.src(['inno-helper.js'])
-        .pipe(filever().on('error', errorHandler));
+    return gulp.src(paths.js)
+        .pipe(filever().on('error', errorHandler))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 }
+
+gulp.task('js:lint', function () {
+    return gulp.src(paths.js)
+        .pipe(jscs())
+        .pipe(jscs.reporter())
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
 gulp.task('js:min', function () {
     return js()
@@ -33,7 +50,7 @@ gulp.task('js:normal', function () {
 });
 
 gulp.task('scss', function () {
-    return gulp.src(['inno-theme.scss'])
+    return gulp.src(paths.scss)
         .pipe(filever().on('error', errorHandler))
         .pipe(sass().on('error', errorHandler))
         .pipe(base64().on('error', errorHandler))
@@ -42,8 +59,8 @@ gulp.task('scss', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['inno-helper.js'], ['js:normal', 'js:min']);
-    gulp.watch(['inno-theme.scss'], ['scss']);
+    gulp.watch(paths.js, ['js:normal', 'js:min', 'js:lint']);
+    gulp.watch(paths.scss, ['scss']);
 });
 
-gulp.task('default', ['js:normal', 'js:min', 'scss', 'watch']);
+gulp.task('default', ['js:normal', 'js:min', 'js:lint', 'scss', 'watch']);
