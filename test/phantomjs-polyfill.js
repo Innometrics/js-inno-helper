fbc914d395d7aecb9f9b3d11806b9e26c0a882d2
Kuvakin Sergey <kuvakin@gmail.com>
2016-01-29 15:27:30 +0300

    fix jscs

--- test/phantomjs-polyfill.js
+++ test/phantomjs-polyfill.js
@@ -2,16 +2,16 @@ if (typeof Function.prototype.bind !== 'function') {
     Function.prototype.bind = function bind (obj) {
         var args = Array.prototype.slice.call(arguments, 1),
             self = this,
-            nop = function () {},
+            Nop = function () {},
             bound = function () {
                 return self.apply(
-                    this instanceof nop ? this : (obj || {}), args.concat(
+                    this instanceof Nop ? this : (obj || {}), args.concat(
                         Array.prototype.slice.call(arguments)
                     )
                 );
             };
-        nop.prototype = this.prototype || {};
-        bound.prototype = new nop();
+        Nop.prototype = this.prototype || {};
+        bound.prototype = new Nop();
         return bound;
     };
 }
